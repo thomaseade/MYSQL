01b1736b6614e9dfd151d9e1dc15e3b8846b1d9c
thomaseade <eadethomas95@gmail.com>
2023-06-29 14:27:33 +0200

    reto1 terminado

--- src/controller/users.controller.js
+++ src/controller/users.controller.js
@@ -1,5 +1,10 @@
 const { pool } =  require('../database');
 
+
+
+//obtener lista alumnos
+
+
 async function getUser(req, res) { 
     let sql ='SELECT * FROM students;'
 
@@ -16,6 +21,7 @@ async function getUser(req, res) {
     }
 };
 
+//Obtener alumnos
 
 async function getUserParams(req, res){
     let sql=`SELECT * FROM students WHERE student_id = ${req.params.student_id};`
@@ -28,10 +34,83 @@ async function getUserParams(req, res){
     }
 };
 
+
+
+// añadir alumno a la lista
+
+
+async function postUser(req, res) {
+    const sql = `INSERT INTO students
+                 (first_name, last_name, id_group, año_ingreso)
+                  VALUES (?, ?, ?, ?);`
+
+    const {first_name, last_name, id_group, año_ingreso} = req.body
+    const params = [first_name, last_name, id_group, año_ingreso]
+
+    try {
+        const [result] = await pool.query(sql,params)
+        res.send(result);
+    } catch (error) {
+        res.send(error);
+    }
+};
+
+// editar alumno
+
+async function putUser(req, res) {
+    const sql = `UPDATE students
+                 SET first_name = COALESCE(?,first_name),
+                 last_name = COALESCE(?,last_name),
+                 id_group = COALESCE(?, id_group),
+                 año_ingreso = COALESCE(?, año_ingreso) 
+                 WHERE student_id = ?;`
+
+const {student_id, first_name, last_name, id_group, año_ingreso } = req.body
+const params = [
+    first_name? first_name:     null,
+    last_name? last_name:       null,
+    id_group? id_group:         null,
+    año_ingreso? año_ingreso:   null,
+    student_id
+] 
   
+try {
+    console.log(sql);
+    const [result] = await pool.query(sql,params)
+    res.send(result);
+} catch (error) {
+    res.send(error); 
+}
+}; 
+
+
+
+
+
+//borrar alumno
+
+async function deleteUser(req, res) {
+    let sql ='DELETE FROM students WHERE student_id = ?;'
+
+    const {student_id} = req.body
+    const params = [student_id]
+
+
+    try {
+        console.log(sql);
+        const [result] = await pool.query(sql,params)
+        res.send(result);
+    } catch (error) {
+        res.send(error); 
+    }
+}
+ 
 
   
   module.exports = {
     getUser,
     getUserParams,
+    postUser,
+    putUser,
+    deleteUser,
   };
